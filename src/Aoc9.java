002596ebd33a5ff4d74286adf79c4020ff9d2fa9
kuchaaar <kuba_kucharek@wp.pl>
2024-12-09 22:22:38 +0100

    aoc2024 day 09

--- src/Aoc9.java
+++ src/Aoc9.java
@@ -8,61 +8,58 @@ public class Aoc9 {
     public Aoc9(List<Integer> list){
         this.input = list;
     }
-    public long part1(){
-        long result =0;
-        List<Integer> listCopy = new ArrayList<>(input);
-        int L = 0;
-        int R = listCopy.size()-1;
-        while(L<R){
-            if(listCopy.get(L) == -1 && listCopy.get(R) != -1){
-                Collections.swap(listCopy,L,R);
-            }
-            else if(listCopy.get(L) != -1){
-                L++;
-            }
-            else if(listCopy.get(R) == -1){
-                R--;
-            }
-            else{
-                L++;
-                R--;
-            }
-        }
-        for(int j=0;j<listCopy.size();j++){
-            if(listCopy.get(j) == -1){
-                break;
-            }
-            else{
-                result = result + (listCopy.get(j) * j);
-            }
-        }
-        return result;
-    }
-    public long part2(){
-        long result = 0;
-        List<Pair<Integer, Integer>> pairs = convertToPairList(input);
+    public long part(boolean part1){
+        List<Pair<Integer,Integer>> pairs = convertToPairList(input);
         for(int i=pairs.size()-1;i>=0;i--){
             for(int j=0;j<i;j++){
                 if(pairs.get(j).getKey() == -1 && pairs.get(i).getKey() != -1){
-                    if(pairs.get(i).getValue() == pairs.get(j).getValue()){
+                    Pair<Integer,Integer> val = pairs.get(i);
+                    Pair<Integer,Integer> blank = pairs.get(j);
+                    if(val.getValue() == blank.getValue()){
                         Collections.swap(pairs,j,i);
                         break;
                     }
-                    else if(pairs.get(i).getValue() < pairs.get(j).getValue()){
-                        Pair<Integer,Integer> val = pairs.get(i);
-                        pairs.set(j,Pair.of(-1,pairs.get(j).getValue() - pairs.get(i).getValue()));
-                        pairs.set(i,Pair.of(-1,val.getValue()));
-                        pairs.add(j,val);
-                        i++;
-                        break;
+                    else{
+                        if(part1){
+                            int a =0;
+                            while(blank.getValue() !=0 && val.getValue() !=0){
+                                pairs.set(j,Pair.of(-1,blank.getValue()-1));
+                                pairs.set(i,Pair.of(val.getKey(),val.getValue() -1));
+                                blank = Pair.of(blank.getKey(),blank.getValue() -1);
+                                val = Pair.of(val.getKey(),val.getValue() -1);
+                                a++;
+                            }
+                            if(blank.getValue() ==0){
+                                pairs.remove(j);
+                                pairs.add(j,Pair.of(val.getKey(),a));
+                            }
+                            if(val.getValue() ==0){
+                                pairs.remove(i);
+                                pairs.add(j,Pair.of(val.getKey(),a));
+                            }
+                        }
+                        else{
+                            if(pairs.get(i).getValue() < pairs.get(j).getValue()){
+                                pairs.set(j,Pair.of(-1,pairs.get(j).getValue() - pairs.get(i).getValue()));
+                                pairs.set(i,Pair.of(-1,val.getValue()));
+                                pairs.add(j,val);
+                                i++;
+                                break;
+                            }
+                        }
                     }
                 }
             }
         }
+        return counter(pairs);
+    }
+
+    public long counter(List<Pair<Integer, Integer>> pairs){
+        long result = 0;
         List<Integer> list = new ArrayList<>();
-        for(int i=0;i<pairs.size();i++){
-            for(int j=0;j<pairs.get(i).getValue();j++){
-                list.add(pairs.get(i).getKey());
+        for (Pair<Integer, Integer> pair : pairs) {
+            for (int j = 0; j < pair.getValue(); j++) {
+                list.add(pair.getKey());
             }
         }
         for(int i=0;i<list.size();i++){
