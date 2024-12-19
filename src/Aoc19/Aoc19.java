7e199ad870dfe261ff4823de30e38b7326f110b6
kuchaaar <kuba_kucharek@wp.pl>
2024-12-19 10:13:13 +0100

    aoc2024 day 19

---
+++ src/Aoc19/Aoc19.java
@@ -0,0 +1,51 @@
+package Aoc19;
+
+import java.util.*;
+
+public class Aoc19 {
+    List<String> towels;
+    List<String> designs;
+
+    public Aoc19(List<String> towels, List<String> designs) {
+        this.towels = towels;
+        towels.sort((a, b) -> b.length() - a.length());
+        this.designs = designs;
+    }
+
+    public String part1(boolean part2) {
+        long res = 0;
+        Map<String, Long> visited = new HashMap<>();
+        for (String design : designs) {
+            visited.clear();
+            long a = dfs(design, visited, new StringBuilder());
+            if (a > 0) {
+                if(part2){
+                    res+=a;
+                }
+                else{
+                    res++;
+                }
+            }
+        }
+        return String.valueOf(res);
+    }
+    private long dfs(String target, Map<String, Long> visited, StringBuilder partial) {
+        if (partial.toString().equals(target)) {
+            return 1L;
+        }
+        if (visited.containsKey(partial.toString())) {
+            return visited.get(partial.toString());
+        }
+        long res = 0;
+        int oldLen = partial.length();
+        for (String towel : towels) {
+            partial.append(towel);
+            if (target.startsWith(partial.toString())) {
+                res += dfs(target, visited, partial);
+            }
+            partial.setLength(oldLen);
+        }
+        visited.put(partial.toString(), res);
+        return res;
+    }
+}
